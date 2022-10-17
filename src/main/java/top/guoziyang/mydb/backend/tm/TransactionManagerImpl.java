443cec0dd29ef21fed5e25e55ec5655bd79cbcfd
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2022-10-17 17:39:28 +0800

    Update TransactionManagerImpl.java

--- src/main/java/top/guoziyang/mydb/backend/tm/TransactionManagerImpl.java
+++ src/main/java/top/guoziyang/mydb/backend/tm/TransactionManagerImpl.java
@@ -1,36 +1,129 @@
 package top.guoziyang.mydb.backend.tm;
 
+import static top.guoziyang.mydb.backend.utils.Types.readByte;
+import static top.guoziyang.mydb.backend.utils.Types.readLong;
+import static top.guoziyang.mydb.backend.utils.Types.writeByte;
 import static top.guoziyang.mydb.backend.utils.Types.writeLong;
 
-import java.io.File;
+import top.guoziyang.mydb.backend.utils.Panic;
+import top.guoziyang.mydb.common.Error;
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public interface TransactionManager {
-    long begin() throws IOException;
-    void commit(long xid) throws IOException;
-    void abort(long xid) throws IOException;
-    boolean isActive(long xid) throws IOException;
-    boolean isCommitted(long xid) throws IOException;
-    boolean isAborted(long xid) throws IOException;
-    void close();
+public class TransactionManagerImpl implements TransactionManager {
 
-    static TransactionManagerImpl create(String path) throws IOException {
-        File f = new File(path + TransactionManagerImpl.XID_SUFFIX);
-        f.createNewFile();
-        RandomAccessFile raf = new RandomAccessFile(f, "rw");
-        FileChannel fc = raf.getChannel();
-        // 写空XID文件头
-        writeLong(fc,0);
-        return new TransactionManagerImpl(raf, fc);
+    // XID文件头长度
+    static final int LEN_XID_HEADER_LENGTH = 8;
+    // 每个事务的占用长度
+    private static final int XID_FIELD_SIZE = 1;
+
+    // 事务的三种状态
+    private static final byte FIELD_TRAN_ACTIVE   = 0;
+	private static final byte FIELD_TRAN_COMMITTED = 1;
+	private static final byte FIELD_TRAN_ABORTED  = 2;
+
+    // 超级事务，永远为commited状态
+    public static final long SUPER_XID = 0;
+
+    static final String XID_SUFFIX = ".xid";
+    
+    private RandomAccessFile file;
+    private FileChannel fc;
+    private long xidCounter;
+    private Lock counterLock;
+
+    TransactionManagerImpl(RandomAccessFile raf, FileChannel fc) throws IOException {
+        this.file = raf;
+        this.fc = fc;
+        counterLock = new ReentrantLock();
+        checkXIDCounter();
     }
 
-    static TransactionManagerImpl open(String path) throws IOException {
-        File f = new File(path + TransactionManagerImpl.XID_SUFFIX);
-        f.createNewFile();
-        RandomAccessFile raf = new RandomAccessFile(f, "rw");
-        FileChannel fc = raf.getChannel();
-        return new TransactionManagerImpl(raf, fc);
+    /**
+     * 检查XID文件是否合法
+     * 读取XID_FILE_HEADER中的xidcounter，根据它计算文件的理论长度，对比实际长度
+     */
+    private void checkXIDCounter() throws IOException {
+        if(file.length() < LEN_XID_HEADER_LENGTH) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+        fc.position(0);
+        this.xidCounter = readLong(fc);
     }
+
+    // 根据事务xid取得其在xid文件中对应的位置
+    private long getXidPosition(long xid) {
+        return LEN_XID_HEADER_LENGTH + (xid-1)*XID_FIELD_SIZE;
+    }
+
+    // 更新xid事务的状态为status
+    private void updateXID(long xid, byte status) throws IOException {
+        fc.position(getXidPosition(xid));
+        writeByte(fc,status);
+    }
+
+    // 将XID加一，并更新XID Header
+    private void incrXIDCounter() throws IOException {
+        xidCounter ++;
+        fc.position(0);
+        writeLong(fc,xidCounter);
+    }
+
+    // 开始一个事务，并返回XID
+    public long begin() throws IOException {
+        counterLock.lock();
+        try {
+            long xid = xidCounter + 1;
+            updateXID(xid, FIELD_TRAN_ACTIVE);
+            incrXIDCounter();
+            return xid;
+        } finally {
+            counterLock.unlock();
+        }
+    }
+
+    // 提交XID事务
+    public void commit(long xid) throws IOException {
+        updateXID(xid, FIELD_TRAN_COMMITTED);
+    }
+
+    // 回滚XID事务
+    public void abort(long xid) throws IOException {
+        updateXID(xid, FIELD_TRAN_ABORTED);
+    }
+
+    // 检测XID事务是否处于status状态
+    private boolean checkXID(long xid, byte status) throws IOException {
+        fc.position(getXidPosition(xid));
+        return readByte(fc) == status;
+    }
+
+    public boolean isActive(long xid) throws IOException {
+        if(xid == SUPER_XID) return false;
+        return checkXID(xid, FIELD_TRAN_ACTIVE);
+    }
+
+    public boolean isCommitted(long xid) throws IOException {
+        if(xid == SUPER_XID) return true;
+        return checkXID(xid, FIELD_TRAN_COMMITTED);
+    }
+
+    public boolean isAborted(long xid) throws IOException {
+        if(xid == SUPER_XID) return false;
+        return checkXID(xid, FIELD_TRAN_ABORTED);
+    }
+
+    public void close() {
+        try {
+            fc.close();
+            file.close();
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
 }
