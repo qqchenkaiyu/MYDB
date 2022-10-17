2e1fb545d7a188cc0fbf6071221c1f971cbac30e
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2022-10-17 17:33:38 +0800

    Update AbstractCache.java

--- src/main/java/top/guoziyang/mydb/backend/common/AbstractCache.java
+++ src/main/java/top/guoziyang/mydb/backend/common/AbstractCache.java
@@ -1,105 +1,42 @@
 package top.guoziyang.mydb.backend.common;
 
-import java.util.HashMap;
-import java.util.Set;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import java.util.Collection;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
-import top.guoziyang.mydb.common.Error;
 
 /**
  * AbstractCache 实现了一个引用计数策略的缓存
  */
 public abstract class AbstractCache<T> {
-    private HashMap<Long, T> cache;                     // 实际缓存的数据
-    private HashMap<Long, Integer> references;          // 元素的引用个数
-    private HashMap<Long, Boolean> getting;             // 正在获取某资源的线程
-
-    private int maxResource;                            // 缓存的最大缓存资源数
-    private int count = 0;                              // 缓存中元素的个数
+                            // 缓存中元素的个数
     private Lock lock;
-
-    public AbstractCache(int maxResource) {
-        this.maxResource = maxResource;
-        cache = new HashMap<>();
-        references = new HashMap<>();
-        getting = new HashMap<>();
+    private Cache<Long, T> loadingCache;
+    public AbstractCache() {
         lock = new ReentrantLock();
+        //创建guava cache
+        loadingCache = Caffeine.newBuilder()
+            .build();
     }
 
-    protected T get(long key) throws Exception {
-        while(true) {
-            lock.lock();
-            if(getting.containsKey(key)) {
-                // 请求的资源正在被其他线程获取
-                lock.unlock();
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    continue;
-                }
-                continue;
+    protected T get(long key) {
+        return loadingCache.get(key, s-> {
+            try {
+                return getForCache(s);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            if(cache.containsKey(key)) {
-                // 资源在缓存中，直接返回
-                T obj = cache.get(key);
-                references.put(key, references.get(key) + 1);
-                lock.unlock();
-                return obj;
-            }
-
-            // 尝试获取该资源
-            if(maxResource > 0 && count == maxResource) {
-                lock.unlock();
-                throw Error.CacheFullException;
-            }
-            count ++;
-            getting.put(key, true);
-            lock.unlock();
-            break;
-        }
-
-        T obj = null;
-        try {
-            obj = getForCache(key);
-        } catch(Exception e) {
-            lock.lock();
-            count --;
-            getting.remove(key);
-            lock.unlock();
-            throw e;
-        }
-
-        lock.lock();
-        getting.remove(key);
-        cache.put(key, obj);
-        references.put(key, 1);
-        lock.unlock();
-        
-        return obj;
+            return null;
+        });
     }
 
     /**
      * 强行释放一个缓存
      */
     protected void release(long key) {
-        lock.lock();
-        try {
-            int ref = references.get(key)-1;
-            if(ref == 0) {
-                T obj = cache.get(key);
-                releaseForCache(obj);
-                references.remove(key);
-                cache.remove(key);
-                count --;
-            } else {
-                references.put(key, ref);
-            }
-        } finally {
-            lock.unlock();
-        }
+        releaseForCache(get(key));
     }
 
     /**
@@ -108,12 +45,9 @@ public abstract class AbstractCache<T> {
     protected void close() {
         lock.lock();
         try {
-            Set<Long> keys = cache.keySet();
-            for (long key : keys) {
-                T obj = cache.get(key);
-                releaseForCache(obj);
-                references.remove(key);
-                cache.remove(key);
+            Collection<T> values = loadingCache.asMap().values();
+            for (T value : values) {
+                releaseForCache(value);
             }
         } finally {
             lock.unlock();
