8d8629b1a0832998be5c053a30fb587f486dec36
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2022-10-17 17:35:20 +0800

    Update Logger.java

--- src/main/java/top/guoziyang/mydb/backend/dm/logger/Logger.java
+++ src/main/java/top/guoziyang/mydb/backend/dm/logger/Logger.java
@@ -1,78 +1,39 @@
 package top.guoziyang.mydb.backend.dm.logger;
 
+import cn.hutool.core.io.FileUtil;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
-import top.guoziyang.mydb.backend.utils.Panic;
-import top.guoziyang.mydb.backend.utils.Parser;
-import top.guoziyang.mydb.common.Error;
-
 public interface Logger {
     void log(byte[] data);
-    void truncate(long x) throws Exception;
     byte[] next();
     void rewind();
     void close();
 
-    public static Logger create(String path) {
-        File f = new File(path+LoggerImpl.LOG_SUFFIX);
-        try {
-            if(!f.createNewFile()) {
-                Panic.panic(Error.FileExistsException);
-            }
-        } catch (Exception e) {
-            Panic.panic(e);
+    static Logger create(String path) throws IOException {
+        File f = FileUtil.file(path + LoggerImpl.LOG_SUFFIX);
+        if (f.exists()) {
+            f.delete();
         }
-        if(!f.canRead() || !f.canWrite()) {
-            Panic.panic(Error.FileCannotRWException);
-        }
-
-        FileChannel fc = null;
-        RandomAccessFile raf = null;
-        try {
-            raf = new RandomAccessFile(f, "rw");
-            fc = raf.getChannel();
-        } catch (FileNotFoundException e) {
-           Panic.panic(e);
-        }
-
-        ByteBuffer buf = ByteBuffer.wrap(Parser.int2Byte(0));
-        try {
-            fc.position(0);
-            fc.write(buf);
-            fc.force(false);
-        } catch (IOException e) {
-            Panic.panic(e);
-        }
-
-        return new LoggerImpl(raf, fc, 0);
+        f.createNewFile();
+        RandomAccessFile raf = new RandomAccessFile(f, "rw");
+        FileChannel fc = raf.getChannel();
+        // 初始化写入空
+        fc.write(ByteBuffer.allocate(4));
+        fc.force(false);
+        return new LoggerImpl(raf, fc);
     }
 
-    public static Logger open(String path) {
-        File f = new File(path+LoggerImpl.LOG_SUFFIX);
-        if(!f.exists()) {
-            Panic.panic(Error.FileNotExistsException);
-        }
-        if(!f.canRead() || !f.canWrite()) {
-            Panic.panic(Error.FileCannotRWException);
-        }
-
-        FileChannel fc = null;
-        RandomAccessFile raf = null;
-        try {
-            raf = new RandomAccessFile(f, "rw");
-            fc = raf.getChannel();
-        } catch (FileNotFoundException e) {
-           Panic.panic(e);
-        }
-
+    static Logger open(String path) throws IOException {
+        File f=FileUtil.file(path+LoggerImpl.LOG_SUFFIX);
+        RandomAccessFile raf = new RandomAccessFile(f, "rw");
+        FileChannel fc = raf.getChannel();
         LoggerImpl lg = new LoggerImpl(raf, fc);
         lg.init();
-
         return lg;
     }
 }
