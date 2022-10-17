bca499daf1b569316a0bce3e715316a1893e7c46
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2022-10-17 17:36:20 +0800

    Update PageCache.java

--- src/main/java/top/guoziyang/mydb/backend/dm/pageCache/PageCache.java
+++ src/main/java/top/guoziyang/mydb/backend/dm/pageCache/PageCache.java
@@ -1,19 +1,18 @@
 package top.guoziyang.mydb.backend.dm.pageCache;
 
+import cn.hutool.core.io.FileUtil;
+import top.guoziyang.mydb.backend.dm.page.Page;
+
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
-
-import top.guoziyang.mydb.backend.dm.page.Page;
-import top.guoziyang.mydb.backend.utils.Panic;
-import top.guoziyang.mydb.common.Error;
-
+// 这里的缓存 就可以用弱一级 的 软引用
 public interface PageCache {
     
     public static final int PAGE_SIZE = 1 << 13;
 
-    int newPage(byte[] initData);
+    Page newPage(byte[] initData);
     Page getPage(int pgno) throws Exception;
     void close();
     void release(Page page);
@@ -22,47 +21,13 @@ public interface PageCache {
     int getPageNumber();
     void flushPage(Page pg);
 
-    public static PageCacheImpl create(String path, long memory) {
-        File f = new File(path+PageCacheImpl.DB_SUFFIX);
-        try {
-            if(!f.createNewFile()) {
-                Panic.panic(Error.FileExistsException);
-            }
-        } catch (Exception e) {
-            Panic.panic(e);
+     static PageCacheImpl open(String path) throws IOException {
+        File f = FileUtil.file(path+PageCacheImpl.DB_SUFFIX);
+        if (!f.exists()) {
+            f.createNewFile();
         }
-        if(!f.canRead() || !f.canWrite()) {
-            Panic.panic(Error.FileCannotRWException);
-        }
-
-        FileChannel fc = null;
-        RandomAccessFile raf = null;
-        try {
-            raf = new RandomAccessFile(f, "rw");
-            fc = raf.getChannel();
-        } catch (FileNotFoundException e) {
-           Panic.panic(e);
-        }
-        return new PageCacheImpl(raf, fc, (int)memory/PAGE_SIZE);
-    }
-
-    public static PageCacheImpl open(String path, long memory) {
-        File f = new File(path+PageCacheImpl.DB_SUFFIX);
-        if(!f.exists()) {
-            Panic.panic(Error.FileNotExistsException);
-        }
-        if(!f.canRead() || !f.canWrite()) {
-            Panic.panic(Error.FileCannotRWException);
-        }
-
-        FileChannel fc = null;
-        RandomAccessFile raf = null;
-        try {
-            raf = new RandomAccessFile(f, "rw");
-            fc = raf.getChannel();
-        } catch (FileNotFoundException e) {
-           Panic.panic(e);
-        }
-        return new PageCacheImpl(raf, fc, (int)memory/PAGE_SIZE);
+        RandomAccessFile raf = new RandomAccessFile(f, "rw");
+        FileChannel fc = raf.getChannel();
+        return new PageCacheImpl(raf, fc);
     }
 }
