cff08605cb2d9975236d7ec083ed670516aa1906
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2022-10-17 17:40:08 +0800

    Update Types.java

--- src/main/java/top/guoziyang/mydb/backend/utils/Types.java
+++ src/main/java/top/guoziyang/mydb/backend/utils/Types.java
@@ -1,9 +1,34 @@
 package top.guoziyang.mydb.backend.utils;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class Types {
     public static long addressToUid(int pgno, short offset) {
-        long u0 = (long)pgno;
-        long u1 = (long)offset;
-        return u0 << 32 | u1;
+        return pgno << 32 | offset;
+    }
+
+    public static long readLong(FileChannel fc) throws IOException {
+        ByteBuffer buf = ByteBuffer.allocate(8);
+        fc.read(buf);
+        return buf.getLong(0);
+    }
+    public static void writeLong(FileChannel fc,long value) throws IOException {
+        ByteBuffer buf = ByteBuffer.allocate(8);
+        buf.putLong(value).rewind();
+        fc.write(buf);
+    }
+
+    public static byte readByte(FileChannel fc) throws IOException {
+        ByteBuffer buf = ByteBuffer.allocate(1);
+        fc.read(buf);
+        return buf.get(0);
+    }
+
+    public static void writeByte(FileChannel fc,byte value) throws IOException {
+        ByteBuffer buf = ByteBuffer.allocate(1);
+        buf.put(value).rewind();
+        fc.write(buf);
     }
 }
